00b8b5364c65206968df6efec76a97279ba057e3
Marcin Kaszynski <marcin.kaszynski@gmail.com>
2017-10-07 15:16:04 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+setup(
+    name = 'redis-rpc',
+    packages = ['redis-rpc'],
+    version = '0.1',
+    description = 'Minimalistic rpc-over-redis library',
+    author = 'Marcin Kaszynski',
+    author_email = 'marcin.kaszynski@gmail.com',
+    url = 'https://github.com/marcinkaszynski/redis-rpc',
+    licene = 'MIT',
+)
