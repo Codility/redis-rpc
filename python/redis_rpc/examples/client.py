83e8d4dac8c740ce118cc3fc09ff9eca0297f23b
Marcin Kaszynski <marcin.kaszynski@gmail.com>
2017-10-11 14:39:46 +0200

    Handle complex objects in client.py

--- python/redis_rpc/examples/client.py
+++ python/redis_rpc/examples/client.py
@@ -1,20 +1,34 @@
 #!env python3
 
+import json
 import sys
 from redis import StrictRedis
 from redis_rpc import Client
 
 
+USAGE = """Usage:
+
+  client get <keyname>
+  client set <keyname> <json-value>
+
+Examples:
+  client get mykey
+  client set mykey 123
+  client set mykey '"text"'
+  client set mykey '{"complex": {"data": "structure"}}'
+"""
+
+
 def main():
     redis = StrictRedis.from_url("redis://localhost:6379/0", socket_timeout=10)
-    cli = Client(redis, 'rpc_example')
+    cli = Client(redis)
 
     if sys.argv[1] == 'get':
-        print(cli.call('get', k=sys.argv[2]))
+        print(json.dumps(cli.call('get', k=sys.argv[2])))
     elif sys.argv[1] == 'set':
-        print(cli.call('set', k=sys.argv[2], v=sys.argv[3]))
+        print(cli.call('set', k=sys.argv[2], v=json.loads(sys.argv[3])))
     else:
-        print("Unknown command:", sys.argv[1])
+        print(USAGE)
 
 
 if __name__ == '__main__':
