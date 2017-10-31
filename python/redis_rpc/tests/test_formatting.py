1e44f27c40175a79d373b3faf09514ba0887a467
Marcin Kaszynski <marcin.kaszynski@gmail.com>
2017-10-31 17:54:32 +0100

    Make YAPF-detected issues easier to understand.

--- python/redis_rpc/tests/test_formatting.py
+++ python/redis_rpc/tests/test_formatting.py
@@ -7,4 +7,6 @@ PACKAGE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../..')
 
 def test_formatting():
     files = glob.glob("%s/**/*.py" % PACKAGE_DIR, recursive=True)
-    assert 0 == yapf.main(['arg0-placeholder', '--diff'] + files)
+    if yapf.main(['arg0-placeholder', '--diff'] + files) != 0:
+        raise Exception("Please use yapf to format code before submitting.  "
+                        "See the diff for details.")
